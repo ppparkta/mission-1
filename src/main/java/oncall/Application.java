d39385be8a359ac325e662a6a443cc8062b7765d
uniiii <126189239+fjqmqjrm@users.noreply.github.com>
2024-11-22 20:24:06 +0900

    feat: 콘솔 닫기 및 해결 전략 작성

--- src/main/java/oncall/Application.java
+++ src/main/java/oncall/Application.java
@@ -1,14 +1,20 @@
 package oncall;
 
 
+import camp.nextstep.edu.missionutils.Console;
 import oncall.controller.WorkScheduleController;
 import oncall.util.InputManager;
 
 public class Application {
 
     public static void main(String[] args) {
-        InputManager inputManager = new InputManager();
-        WorkScheduleController workScheduleController = new WorkScheduleController(inputManager);
-        workScheduleController.run();
+        try {
+            InputManager inputManager = new InputManager();
+            WorkScheduleController workScheduleController = new WorkScheduleController(inputManager);
+            workScheduleController.run();
+        }
+        finally {
+            Console.close();
+        }
     }
 }
