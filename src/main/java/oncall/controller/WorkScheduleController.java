cd37b6058b3436c1573549999014d9c51f91b89a
uniiii <126189239+fjqmqjrm@users.noreply.github.com>
2024-11-22 20:19:24 +0900

    refactor: 요일별 순회 메서드 분리

--- src/main/java/oncall/controller/WorkScheduleController.java
+++ src/main/java/oncall/controller/WorkScheduleController.java
@@ -3,7 +3,6 @@ package oncall.controller;
 import oncall.domain.Date;
 import oncall.domain.Day;
 import oncall.domain.WorkPermutation;
-import oncall.domain.Worker;
 import oncall.util.InputManager;
 
 import java.util.List;
@@ -23,6 +22,7 @@ public class WorkScheduleController {
         WorkPermutation holidayPermutation = workPermutationWeekAndHoliday.get(1);
         calculateWorkSchedule(startDate, weekPermutation, holidayPermutation);
     }
+
     // 모든 요일 순회 및  관장
     private void calculateWorkSchedule(Date startDate, WorkPermutation weekPermutation, WorkPermutation holidayPermutation) {
         int lastDay = startDate.getLastDay();
@@ -31,6 +31,13 @@ public class WorkScheduleController {
         List<Day> days = Day.getDays(); // 요일 리스트
         int dayPoint = days.indexOf(startDate.getDay()); // 시작 요일 지점
         String yesterDayWorkername = "";
+        processSchedule(startDate, lastDay, weekTurnPoint, holidayTurnPoint, days, dayPoint, yesterDayWorkername, weekPermutation, holidayPermutation);
+
+    }
+
+    private void processSchedule(Date startDate, int lastDay, int weekTurnPoint, int holidayTurnPoint,
+                                 List<Day> days, int dayPoint, String yesterDayWorkername,
+                                 WorkPermutation weekPermutation, WorkPermutation holidayPermutation) {
         // 일 별 절차
         for (int i = 1; i <= lastDay; i++) {
             Date pointDate = new Date(startDate.getMonth(), days.get(dayPoint)); // 해당 일 정보 생성
@@ -38,19 +45,23 @@ public class WorkScheduleController {
             if (isHoliday) {
                 String workerName = holidayPermutation.getWorkers().get(holidayTurnPoint).getName();
                 if (workerName.equals(yesterDayWorkername)) {
-                   holidayPermutation.workkerChange(holidayTurnPoint);
-                   workerName = holidayPermutation.getWorkers().get(holidayTurnPoint).getName();
+                    holidayPermutation.workkerChange(holidayTurnPoint);
+                    workerName = holidayPermutation.getWorkers().get(holidayTurnPoint).getName();
                 }
                 holidayTurnPoint++;
-                if (pointDate.isWeekend(i)){
-                    System.out.println(pointDate.getMonth()+ "월 " + i +"일 " + Day.getDayString(dayPoint) + " " + workerName);
+                if (pointDate.isWeekend(i)) {
+                    System.out.println(pointDate.getMonth() + "월 " + i + "일 " + Day.getDayString(dayPoint) + " " + workerName);
                 }
-                if (!(pointDate.isWeekend(i))){
-                    System.out.println(pointDate.getMonth()+ "월 " + i +"일 " + Day.getDayString(dayPoint) + "(휴일) " + workerName);
+                if (!(pointDate.isWeekend(i))) {
+                    System.out.println(pointDate.getMonth() + "월 " + i + "일 " + Day.getDayString(dayPoint) + "(휴일) " + workerName);
                 }
-                if (holidayPermutation.getWorkers().size()-1 == holidayTurnPoint) {holidayTurnPoint = 0;}
-                dayPoint ++;
-                if (dayPoint == 7) {dayPoint = 0;}
+                if (holidayPermutation.getWorkers().size() - 1 == holidayTurnPoint) {
+                    holidayTurnPoint = 0;
+                }
+                dayPoint++;
+                if (dayPoint == 7) {
+                    dayPoint = 0;
+                }
                 yesterDayWorkername = workerName;
             }
             if (!isHoliday) {
@@ -60,18 +71,19 @@ public class WorkScheduleController {
                     workerName = weekPermutation.getWorkers().get(holidayTurnPoint).getName();
                 }
                 weekTurnPoint++;
-                System.out.println(pointDate.getMonth()+ "월 " + i +"일 " + Day.getDayString(dayPoint) + " " + workerName);
-                if (weekPermutation.getWorkers().size()-1 == weekTurnPoint) {weekTurnPoint = 0;}
-                dayPoint ++;
-                if (dayPoint == 7) {dayPoint = 0;}
+                System.out.println(pointDate.getMonth() + "월 " + i + "일 " + Day.getDayString(dayPoint) + " " + workerName);
+                if (weekPermutation.getWorkers().size() - 1 == weekTurnPoint) {
+                    weekTurnPoint = 0;
+                }
+                dayPoint++;
+                if (dayPoint == 7) {
+                    dayPoint = 0;
+                }
                 yesterDayWorkername = workerName;
-
             }
 
-
         }
+
     }
-
-
 
 }
